67a91c8a979d27310a992dd61516ad87526df9b5
Michal <hello@michalk.nl>
2025-02-06 15:45:41 +0100

    (echant) Added rank to the top right.

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -29,7 +29,7 @@
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-hidden transition ease-in-out duration-150">
-                            <div>{{ Auth::user()->name }}</div>
+                            <div>{{ Auth::user()->name }} | <span class="@if(Auth::user()->role === "Admin") text-blue-500 @elseif(Auth::user()->role === "Staff") text-green-500 @else text-gray-400 @endif">{{Auth::user()->role}}</span></div>
 
                             <div class="ms-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
