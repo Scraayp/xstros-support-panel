39fa4864154108d41df980274623201cdefe5901
Michal <hello@michalk.nl>
2025-02-06 10:05:55 +0100

    Remove welcome view from the application
    The `welcome.blade.php` file has been deleted. This removes the default Laravel welcome page, likely in preparation for a custom landing page or updated views.

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,6 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
@@ -16,5 +12,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::get('/user/list', [\App\Http\Controllers\UserController::class, 'view'])->middleware(['auth', 'verified'])->name('user.list');
 
 require __DIR__.'/auth.php';
